8bfaeefcf661161240aed715df564146389c7865
Void GH <moltron_nano@hotmail.com>
2021-08-21 10:49:34 -0600

    Initial version of kafka consumer app

---
+++ src/main/java/com/tudux/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,10 @@
+package com.tudux.libraryeventsconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+
+}
